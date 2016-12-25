bea50b5ea34a686fc30de4045b2d579e1a596117
Siker001 <sikergarantalt@gmail.com>
2016-12-25 20:47:35 +0100

    Add notes to substrings and readme.

---
+++ ruby/building_blocks/substrings/README.md
@@ -0,0 +1,4 @@
+Solution to Substrings
+Giving a string to the method and method checkes how many words matches in the string in its dictionary words.
+
+http://www.theodinproject.com/courses/ruby-programming/lessons/building-blocks?ref=lnav
--- ruby/building_blocks/substrings/substrings.rb
+++ ruby/building_blocks/substrings/substrings.rb
@@ -1,13 +1,15 @@
 # Substrings
+# Giving a string to the method and method checkes how many words matches in the string in its dictionary words.
+
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(word, dictionary)
 	$result = {}
-	more = word.downcase.split(" ")
-	more.each do |y|
-		dictionary.each do |x|
-			if (y.include? x)
-				$result[x] = $result[x].to_i + 1
+	more = word.downcase.split(" ") 	# Change to non case sensitiveness with converting the string to downcase and then slitting it up into words into an array.
+	more.each do |y| 					# Iterating through each word.
+		dictionary.each do |x| 			# Iterating through the dictionary one by one and checking each one of them with the words.
+			if (y.include? x) 			# Checking if the given word is including the given dictionary's word(x).
+				$result[x] = $result[x].to_i + 1 		# If it does include, then add it to the $result hash with the hashkey of the given dictionary's word(x) and moving up its value number by 1.
 			end
 		end
 	end
