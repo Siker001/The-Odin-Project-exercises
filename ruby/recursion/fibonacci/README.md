476798c49316cbc309df41b546378a6eee44383e
Siker001 <sikergarantalt@gmail.com>
2017-01-09 21:16:34 +0100

    Add comments.

--- ruby/recursion/fibonacci/README.md
+++ ruby/recursion/fibonacci/README.md
@@ -1,4 +1,6 @@
 This is a solution to the Fibonacci exercise which is part of the Odin Project.
 
+The recursive solution was inspired by https://abhibhagat.wordpress.com/2010/03/16/fibonacci-series-using-recursion-in-ruby/
+
 Source:
 http://www.theodinproject.com/courses/ruby-programming/lessons/recursion?ref=lnav
--- ruby/recursion/fibonacci/fibonacci.rb
+++ ruby/recursion/fibonacci/fibonacci.rb
@@ -1,3 +1,5 @@
+# Looping through n-1 times, and adding the last two values of the array together.
+# This gives the fibonacci sequence at the end.
 def fib(arg)
 	answ = []
 	if arg == 0
@@ -24,7 +26,12 @@ fib(6)
 fib(7)
 fib(10)
 
-
+# This is a recursive method to print out the fibonacci sequence.
+# Accepts and needs 1srt(i), 2nd(j) value, a counter(cnt) and n as how many fibonacci numbers we would like from the sequence.
+# When method runs more than n, it returns with the last number of the fibonacci sequence.
+# Add 1st and 2nd number together, you get the 3rd one like this. Which will become the 2nd number in the next method.
+# As the current 2nd will be set to be the 1st number in the next method.
+# Also counter increses by one, and n stays the same.
 def fibs_rec(i, j, cnt, n)
     if(cnt > n)
        return i
@@ -35,7 +42,6 @@ def fibs_rec(i, j, cnt, n)
        j = k
        fibs_rec(i, j, cnt += 1, n)
     end
-
 end
 
 fibs_rec(0, 1, 0, 0)
