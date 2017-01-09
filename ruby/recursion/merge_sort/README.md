5b1e531ad5ddd57a8efdf6ddff13815f110750e9
Siker001 <sikergarantalt@gmail.com>
2017-01-09 21:16:03 +0100

    Add notes

---
+++ ruby/recursion/merge_sort/README.md
@@ -0,0 +1,4 @@
+Solution to merge sort method.
+
+Source:
+http://www.theodinproject.com/courses/ruby-programming/lessons/recursion?ref=lnav
--- ruby/recursion/merge_sort/merge_sort.rb
+++ ruby/recursion/merge_sort/merge_sort.rb
@@ -1,4 +1,6 @@
-
+# Merge sorting method in 2 parts.
+# First we split the array into half with recursion till we have only 1 element in array.
+# The whole recusive method for both half goes under merge method as 2 parameters.
 def merge_sort(arr)
 	if arr.length == 1
 		return arr
@@ -8,6 +10,12 @@ def merge_sort(arr)
 	end
 end
 
+# Merge method compares the 2 parameters passed into it.
+# Until one of the parameters array.length becomes 0, the comparison will run.
+# If left array's first element is smaller than right's then remove it from the array, store it in smaller variable.
+# Then push this into result array.
+# This loops until one of the array runs out of elements.
+# Which point array concatination happens and we just add them all together. Since they are sorted.
 def merge(left, right)
 	result = []
 	until left.length == 0 || right.length == 0
